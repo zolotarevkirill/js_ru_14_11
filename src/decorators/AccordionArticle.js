ded9260005832accbd909c050f5fafefdde4f7ac
Roman <kaboom.ua@gmail.com>
2016-11-23 21:36:39 +0200

    Update AccordionArticle.js

--- src/decorators/AccordionArticle.js
+++ src/decorators/AccordionArticle.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 export default (Component) => class Accordion extends React.Component {
   state = {
+    //не привязывайся к названиям сущностей в декораторах, вся их суть в том, чтобы использовать с разными компонентами и данными. Сделай openItemId
+    // + prevArticleId лишнее для этой задачи
     prevArticleId: null,
     openArticleId: null
   }
