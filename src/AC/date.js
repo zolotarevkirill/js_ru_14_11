d6d41720a2c63e9d159fd60129fc1e3ccc2ddb96
Roman <kaboom.ua@gmail.com>
2016-12-01 20:23:53 +0200

    Update date.js

--- src/AC/date.js
+++ src/AC/date.js
@@ -1,5 +1,5 @@
 import { SET_DATE_FILTER } from '../constants'
-//лучше объеденить эти 2 AC
+//лучше объеденить эти 2 AC в один файл
 export function dateRange ({from, to}) {
     return {
         type: SET_DATE_FILTER,
